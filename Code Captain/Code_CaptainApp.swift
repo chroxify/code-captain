e235fefe3852b9401b4a280b446c2609f880f141
Christo Todorov <chroxify@gmail.com>
2025-07-17 22:33:40 +0200

    Initial Commit

---
+++ Code Captain/Code_CaptainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_CaptainApp.swift
+//  Code Captain
+//
+//  Created by Christo Todorov on 17.07.25.
+//
+
+import SwiftUI
+
+@main
+struct Code_CaptainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Code CaptainTests/Code_CaptainTests.swift
@@ -0,0 +1,17 @@
+//
+//  Code_CaptainTests.swift
+//  Code CaptainTests
+//
+//  Created by Christo Todorov on 17.07.25.
+//
+
+import Testing
+@testable import Code_Captain
+
+struct Code_CaptainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
